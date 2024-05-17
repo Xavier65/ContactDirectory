f5590a91b0d7a5ad4b3fd86551eb0bcae56c5cb3
Xavier Hernandez <Xavier65>
2024-05-17 16:43:11 -0600

    Change DataManager

--- DataManager.py
+++ DataManager.py
@@ -1,6 +1,24 @@
+import sqlite3
+
+
 class DataManager:
     def __init__(self, database: str) -> None:
-        self.__database_name: str = database
+        self.__connection = sqlite3.connect(database)
+        self.__cursor = self.__connection.cursor()
 
-    def connection(self):
-        pass
+    def commit(self) -> None:
+        self.__connection.commit()
+
+    def getContacts(self) -> list:
+        self.__contacts = self.__cursor.execute("SELECT * FROM Contact;")
+        return self.__contacts.fetchall()
+
+    def getCellphoneNumbers(self) -> list:
+        self.__cellphone_numbers = self.__cursor.execute(
+            "SELECT * FROM CellphoneNumber;"
+        )
+        return self.__cellphone_numbers.fetchall()
+
+    def getAddresses(self) -> list:
+        self.__addresses = self.__cursor.execute("SELECT * FROM Address;")
+        return self.__addresses.fetchall()
